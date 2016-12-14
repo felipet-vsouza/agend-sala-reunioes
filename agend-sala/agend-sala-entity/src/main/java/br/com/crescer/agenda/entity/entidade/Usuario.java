3043b700d142316406766c599c126fe90dc69f2b
Unknown <hhosterman@hotmail.com>
2016-12-14 14:59:10 -0200

    feat(EntityUsuario)

--- agend-sala/agend-sala-entity/src/main/java/br/com/crescer/agenda/entity/entidade/Usuario.java
+++ agend-sala/agend-sala-entity/src/main/java/br/com/crescer/agenda/entity/entidade/Usuario.java
@@ -1,13 +1,67 @@
+
+import java.io.Serializable;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author henrique.ostermann
  */
-public class Usuario {
-    
+@Entity
+@Table(name = "USUARIO")
+public class Usuario implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQ_USUARIO")
+    @SequenceGenerator(name = "SEQ_USUARIO", sequenceName = "SEQ_USUARIO", allocationSize = 1)
+    @Basic(optional = false)
+    @Column(name = "ID_USUARIO")
+    private Long id;
+
+    @Basic(optional = false)
+    @Column(name = "EMAIL_USUARIO")
+    private String email;
+
+    @Basic(optional = false)
+    @Column(name = "SENHA_USUARIO")
+    private String senha;
+
+    @Basic(optional = false)
+    @Column(name = "NOME_USUARIO")
+    private String nome;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
