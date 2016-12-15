887abcdf06c99e93c867913dd2bac35f15f68c04
Felipe Souza <felipe.souza@cwi.com.br>
2016-12-15 13:34:17 -0200

    feat(Usuario.java, UsuarioServicoTest.java): preparar serviço de Usuario e seus testes.

--- agend-sala/agend-sala-service/src/main/java/br/com/crescer/agend/service/UsuarioServico.java
+++ agend-sala/agend-sala-service/src/main/java/br/com/crescer/agend/service/UsuarioServico.java
@@ -8,7 +8,10 @@ package br.com.crescer.agend.service;
 import br.com.crescer.agend.entity.Usuario;
 import br.com.crescer.agend.repository.UsuarioRepositorio;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 /**
  *
@@ -22,5 +25,30 @@ public class UsuarioServico {
 
     public Usuario findByEmail(String username) {
         return usuarioRepositorio.findByEmail(username);
+    }
+    
+    public Usuario save(Usuario usuario) {
+        String senha = usuario.getSenha();
+        senha = new BCryptPasswordEncoder().encode(senha);
+        usuario.setSenha(senha);
+        usuarioRepositorio.save(usuario);
+        return usuario;
+    }
+    
+    public Usuario update(Usuario usuario) {
+        usuarioRepositorio.save(usuario);
+        return usuario;
+    }
+    
+    public Iterable<Usuario> findAll() {
+        return usuarioRepositorio.findAll();
+    }
+    
+    public Page<Usuario> findAll(Pageable pageable) {
+        return usuarioRepositorio.findAll(pageable);
+    }
+    
+    public Usuario findOne(Long id) {
+        return usuarioRepositorio.findOne(id);
     }
 }
