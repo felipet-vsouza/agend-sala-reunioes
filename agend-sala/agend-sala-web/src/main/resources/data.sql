4d21451b8a8ab1be2a6f39444983228bd13307bf
Felipe Souza <felipe.souza@cwi.com.br>
2016-12-15 14:37:50 -0200

    fix(data.sql): corrigir nome de tabela no script de população do banco.

--- agend-sala/agend-sala-web/src/main/resources/data.sql
+++ agend-sala/agend-sala-web/src/main/resources/data.sql
@@ -8,9 +8,9 @@
  * Created: 15/12/2016
  */
 -- -- TABELA USUARIO
-Insert into EXPORT_TABLE (ID_USUARIO,EMAIL_USUARIO,NOME_USUARIO,SENHA_USUARIO) values ('1','felipe.souza@cwi.com.br','Felipe Souza','$2a$10$ofd6tqa.yBM3N0BgNqFbTuBnO3EwoGbx6Np2GizCjMkvJLZbjlxxK');
-Insert into EXPORT_TABLE (ID_USUARIO,EMAIL_USUARIO,NOME_USUARIO,SENHA_USUARIO) values ('2','henrique.ostermann@cwi.com.br','Henrique Ostermann','$2a$10$RIJaNA3vfkRxoSpqeJ83K.cWGACTtve8EfWENLWiSVOOur9DwHKDG');
-Insert into EXPORT_TABLE (ID_USUARIO,EMAIL_USUARIO,NOME_USUARIO,SENHA_USUARIO) values ('3','henrique.mentz@cwi.com.br','Henrique Mentz','$2a$10$V7T2i916qHS6y4ZTOsB6DeySxFYttRsWxb1oj9ZSrYnKlJJ3Z8WRu');
+Insert into USUARIO (ID_USUARIO,EMAIL_USUARIO,NOME_USUARIO,SENHA_USUARIO) values ('1','felipe.souza@cwi.com.br','Felipe Souza','$2a$10$ofd6tqa.yBM3N0BgNqFbTuBnO3EwoGbx6Np2GizCjMkvJLZbjlxxK');
+Insert into USUARIO (ID_USUARIO,EMAIL_USUARIO,NOME_USUARIO,SENHA_USUARIO) values ('2','henrique.ostermann@cwi.com.br','Henrique Ostermann','$2a$10$RIJaNA3vfkRxoSpqeJ83K.cWGACTtve8EfWENLWiSVOOur9DwHKDG');
+Insert into USUARIO (ID_USUARIO,EMAIL_USUARIO,NOME_USUARIO,SENHA_USUARIO) values ('3','henrique.mentz@cwi.com.br','Henrique Mentz','$2a$10$V7T2i916qHS6y4ZTOsB6DeySxFYttRsWxb1oj9ZSrYnKlJJ3Z8WRu');
 
 --TABELA EQUIPAMENTOS   
 INSERT INTO EQUIPAMENTO (ID_EQUIPAMENTO, NOME_EQUIPAMENTO)  VALUES (SEQ_EQUIPAMENTO.nextval, 'TELEVISAO');
