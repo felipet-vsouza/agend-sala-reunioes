201dbf75921852ebb6b0623a18ae33c882875fdb
Felipe Souza <felipe.souza@cwi.com.br>
2016-12-17 16:16:02 -0200

    test(SalaRepositorioTest.java): incluir testes para repositório de sala.

---
+++ agend-sala/agend-sala-service/src/test/java/br/com/crescer/TestRun.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.crescer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author Felps-Notebook
+ */
+@SpringBootApplication
+public class TestRun {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(TestRun.class, args);
+    }
+}
