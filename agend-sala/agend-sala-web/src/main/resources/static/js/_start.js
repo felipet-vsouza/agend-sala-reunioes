aa4086a26ae7814f1a0483dbc0cdd9467e5d369c
Felipe Souza <felipe.souza@cwi.com.br>
2016-12-19 21:53:23 -0200

    feat(_start.js): centralizar todos os arquivos JS em um só.

--- agend-sala/agend-sala-web/src/main/resources/static/js/_start.js
+++ agend-sala/agend-sala-web/src/main/resources/static/js/_start.js
@@ -1,3 +1,4 @@
+// funções iniciais
 var main = {};
 
 main.defineDefaultLoader = function () {
@@ -44,6 +45,66 @@ $(function () {
     new Home();
 });
 
+
+// validações
+var parseBrazilianDate = function (content) {
+    let from = content.split("/");
+    return new Date(from[2], from[1] - 1, from[0]);
+}
+
+var parseHour = function (content) {
+    let hour = parseInt(content.substring(0, 2));
+    let minute = parseInt(content.substring(3, 5));
+    hour += minute / 60;
+    return hour;
+}
+
+$.validator.addMethod("anyDate",
+    function (value, element) {
+        return value.match(/^(0?[1-9]|[12][0-9]|3[0-1])[/., -](0?[1-9]|1[0-2])[/., -](19|20)?\d{2}$/);
+    },
+    "Por favor, insira uma data válida."
+);
+
+$.validator.addMethod("afterCurrentDay",
+    function (value, element) {
+        let atual = new Date();
+        atual.setHours(0);
+        atual.setMinutes(0);
+        atual.setSeconds(0);
+        atual.setMilliseconds(0);
+        let selecionada = parseBrazilianDate(value);
+        return (selecionada >= atual);
+    },
+    "Por favor, selecione uma data após a atual."
+);
+
+$.validator.addMethod("anyHour",
+    function (value, element) {
+        return value.match(/^([0-1]?[0-9]|2[0-4]):([0-5][0-9])(:[0-5][0-9])?$/);
+    },
+    "Por favor, insira um horário válido."
+);
+
+$.validator.addMethod("validHour",
+    function (value, element) {
+        let hour = value.substring(0, 2);
+        return (hour >= 8 && hour <= 22);
+    },
+    "Por favor, insira um horário entre 8h e 22h."
+);
+
+$.validator.addMethod('competesWithOtherHour',
+    function (value, element, params) {
+        var initial = parseHour($(`input[name="${params[0]}"]`).val()),
+            final = parseHour($(`input[name="${params[1]}"]`).val());
+        return (initial < final);
+    },
+    "A hora inicial deve ser anterior à final."
+);
+
+
+// classes de telas
 class Home {
     constructor() {
         this.modal = $('.modal');
@@ -114,7 +175,7 @@ class Home {
                 .then(res => {
                     self.modal.css("display", "flex");
                     self.modalContent.html(res);
-                    new AgendamentoSalas();
+                    new AgendamentoSalas(self);
                 })
                 .fail(err => {
                     self.modal.css("display", "flex");
@@ -139,6 +200,7 @@ class Home {
         $.get('/home/reunioes')
             .then(res => {
                 self.reunioes.html(res);
+                self.defineDetalheAgendamentosBind();
             })
             .fail(err => {
                 console.error('Erro na requisição: ', err);
@@ -165,5 +227,207 @@ class PopupConfirmation {
             .fail(err => {
                 console.error('Erro na requisição: ', err);
             });
+    }
+}
+
+class FiltroSalas {
+    constructor() {
+        this.form = $('#frm-filtro');
+        this.capacity = $('#in-pps');
+        this.date = $('#in-dat');
+        this.horaInicio = $('#in-hri');
+        this.horaFim = $('#in-hrf');
+        this.getButton = $('#bt-get');
+        this.modalContent = $('.modal-content');
+        this.defineFieldFormats();
+        this.defineFormValidation();
+        this.defineButtonBind();
+    }
+
+    defineFieldFormats() {
+        this.date.datepicker({
+            dateFormat: 'dd/mm/yy'
+        });
+        let timepickerConfig = {
+            timeFormat: 'HH:mm',
+            minTime: '07:00',
+            maxTime: '22:00',
+            defaultTime: '13:30',
+            dynamic: false,
+            dropdown: false,
+            scrollbar: false
+        };
+        this.horaInicio.timepicker(timepickerConfig);
+        this.horaFim.timepicker(timepickerConfig);
+    }
+
+    defineFormValidation() {
+        this.form.validate({
+            rules: {
+                quantidade: {
+                    required: true,
+                    min: 0
+                },
+                data: {
+                    required: true,
+                    anyDate: true,
+                    afterCurrentDay: true
+                },
+                inicio: {
+                    required: true,
+                    anyHour: true,
+                    validHour: true,
+                    competesWithOtherHour: ['inicio', 'final']
+                },
+                final: {
+                    required: true,
+                    anyHour: true,
+                    validHour: true,
+                    competesWithOtherHour: ['inicio', 'final']
+                }
+            }
+        });
+    }
+
+    defineButtonBind() {
+        let self = this;
+        this.getButton.click(function () {
+            if (self.form.valid()) {
+                let capacidade = self.capacity.val();
+                capacidade = parseInt(capacidade);
+                let initialDate = self.date.datepicker("getDate");
+                let finalDate = self.date.datepicker("getDate");
+                let hora = self.horaInicio.val().substring(0, 2);
+                let minuto = self.horaInicio.val().substring(3, 5);
+                initialDate.setHours(parseInt(hora));
+                initialDate.setMinutes(parseInt(minuto));
+                hora = self.horaFim.val().substring(0, 2);
+                minuto = self.horaFim.val().substring(3, 5);
+                finalDate.setHours(parseInt(hora));
+                finalDate.setMinutes(parseInt(minuto));
+                var equipamentos = [];
+                $('#in-eqp :checked').each(function () {
+                    equipamentos.push(parseInt($(this).val()));
+                });
+                $.get('/salas/find', {
+                    dataInicio: initialDate,
+                    dataFim: finalDate,
+                    capacidade: capacidade,
+                    equipamentos: equipamentos
+                })
+                    .then(res => {
+                        self.modalContent.html(res);
+                    })
+                    .fail(err => {
+                        console.log(err);
+                        self.modalContent.toggle();
+                    });
+            }
+        });
+    }
+}
+
+class AgendamentoSalas {
+    constructor(container) {
+        this.container = container;
+        this.form = $('#frm-agendamento');
+        this.descricao = $('#ag-des');
+        this.sala = $('#ag-sl');
+        this.name = $('#ag-nm');
+        this.date = $('#ag-dat');
+        this.horaInicio = $('#ag-hri');
+        this.horaFim = $('#ag-hrf');
+        this.postButton = $('#bt-post');
+        this.modalContent = $('.modal-content');
+        this.defineFieldFormats();
+        this.defineFormValidation();
+        this.defineButtonBind();
+    }
+
+    defineFieldFormats() {
+        this.date.datepicker({
+            dateFormat: 'dd/mm/yy'
+        });
+        let timepickerConfig = {
+            timeFormat: 'HH:mm',
+            minTime: '08:00',
+            maxTime: '22:00',
+            defaultTime: '13:30',
+            dynamic: false,
+            dropdown: false,
+            scrollbar: false
+        };
+        this.horaInicio.timepicker(timepickerConfig);
+        this.horaFim.timepicker(timepickerConfig);
+    }
+
+    defineFormValidation() {
+        this.form.validate({
+            rules: {
+                sala: {
+                    required: true
+                },
+                descricao: {
+                    required: true
+                },
+
+                data: {
+                    required: true,
+                    anyDate: true,
+                    afterCurrentDay: true
+                },
+                inicio: {
+                    required: true,
+                    anyHour: true,
+                    validHour: true,
+                    competesWithOtherHour: ['inicio', 'final']
+                },
+                final: {
+                    required: true,
+                    anyHour: true,
+                    validHour: true,
+                    competesWithOtherHour: ['inicio', 'final']
+                }
+            }
+        });
+    }
+
+    defineButtonBind() {
+        let self = this;
+        this.postButton.click(function () {
+            if (self.form.valid()) {
+                let initialDate = self.date.datepicker("getDate");
+                let finalDate = self.date.datepicker("getDate");
+                let hora = self.horaInicio.val().substring(0, 2);
+                let minuto = self.horaInicio.val().substring(3, 5);
+                let descricao = self.descricao.val();
+                initialDate.setHours(parseInt(hora));
+                initialDate.setMinutes(parseInt(minuto));
+                hora = self.horaFim.val().substring(0, 2);
+                minuto = self.horaFim.val().substring(3, 5);
+                finalDate.setHours(parseInt(hora));
+                finalDate.setMinutes(parseInt(minuto));
+                var idSala = parseInt($('#ag-sl option:selected').val());
+                var usuarios = [];
+                $('#ag-nm :checked').each(function () {
+                    usuarios.push(parseInt($(this).val()));
+                });
+                $.post('/agendamento/adicionar', {
+                    idSala: idSala,
+                    descricao: descricao,
+                    dataInicial: initialDate,
+                    dataFinal: finalDate,
+                    usuarios: usuarios
+                })
+                    .then(res => {
+                        self.container.refreshPainelReunioes();
+                        self.modalContent.html(res);
+                    })
+                    .fail(err => {
+                        console.log(err);
+                        self.modalContent.toggle();
+                    });
+            }
+        });
     }
 }
